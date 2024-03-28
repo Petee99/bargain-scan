6208f9434ce99d5d38f637556d3450a4279c74d9
Peter Mako <petee1014@gmail.com>
2024-03-28 23:45:42 +0100

    Added test project

---
+++ MobileAPP/MobileApp/MobileApp.Test/UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="UnitTest1.cs" owner="Peter Mako">
+//   Thesis work by Peter Mako for Obuda University / Business Informatics MSc. 2024
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MobileApp.Test
+{
+    #region Imports
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    #endregion
+
+    [TestClass]
+    public class UnitTest1
+    {
+        #region Public Methods and Operators
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        #endregion
+    }
+}
